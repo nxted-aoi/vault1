8b5e5ae99a53da2494a8e27490e900148b725aa9
aoikurokawa <aoi.kurokawa@nxted.co.jp>
2025-01-10 14:03:13 +0900

    feat: init

---
+++ index.js
@@ -0,0 +1,32 @@
+import { PublicKey } from "@solana/web3.js";
+import { Connection } from "@solana/web3.js";
+
+function getConnection(chain = 'solana') {
+    // if (!connection[chain]) connection[chain] = new Connection(endpointMap[chain](true))
+    return new Connection("https://api.mainnet-beta.solana.com")
+  }
+
+async function tvl() {
+    // https://jito.network/staking
+    const connection = getConnection();
+    const account = await connection.getAccountInfo(new PublicKey('Jito4APyf642JPZPx3hGc6WWJ8zPKtRbRs4P815Awbb'))
+    return {
+      solana: Number(account.data.readBigUint64LE(258))/1e9
+    }
+  }
+
+async function tvl1() {
+    const connection = getConnection();
+    const accounts = await connection.getProgramAccounts(new PublicKey("Vau1t6sLNxnzB7ZDsef8TLbPLfyZMYXH8WTNqUdm9g8 "), {
+        filters: [{
+          dataSize: 1544
+        }]
+      })
+    getAssociatedTokenAddress
+    const vaults = [
+        ""
+    ];
+}
+
+const tvlNumber = await tvl();
+console.log(tvlNumber.solana);
---
+++ vault.js
@@ -0,0 +1,77 @@
+const {
+    struct,
+    s32,
+    u8,
+    u16,
+    seq,
+    blob,
+    Layout,
+    bits,
+    u32,
+    publicKey,
+    uint64,
+    u64,
+    uint128,
+    u128,
+    option,
+} = require("./layout-base");
+
+const JITO_VAULT = struct([
+    u8("discriminator"),
+    publicKey("base"),
+    publicKey("vrtMint"),
+    publicKey("supportedMint"),
+    u64("vrtSupply"),
+    u64("tokensDeposited"),
+    u64("depositCapacity"),
+    struct(DELEGATION_STATE),
+    u64(additionalAssetsNeedUnstaking),
+    u64(vrtEnqueuedForCooldownAmount),
+    u64(vrtCoolingDownAmount),
+    u64(vrtReadyToClaimAmount),
+    publickKey(admin),
+    publicKey(delegationAdmin),
+    publicKey(operatorAdmin),
+    publicKey(ncnAdmin),
+    publicKey(slasherAdmin),
+    publicKey(capacityAdmin),
+    publicKey(feeAdmin),
+    publicKey(delegateAssetAdmin),
+    publicKey(feeWallet),
+    publicKey(mintBurnAdmin),
+    publicKey(metadataAdmin),
+    u64(vaultIndex),
+    u64(ncnCount),
+    u64(operatorCount),
+    u64(slasherCount),
+    u64(lastFeeChangeSlot),
+    u64(lastFullStateUpdateSlot),
+    u16(depositFeeBps),
+    u16(withdrawalFeeBps),
+    u16(nextWithdrawalFeeBps),
+    u16(rewardFeeBps),
+    u16(programFeeBps),
+    u16(bump),
+    Boolean(isPaused),
+    u64(lastStartStateUpdateSlot),
+]);
+
+const DELEGATION_STATE = [
+
+]
+
+const parseSanctumLstStateList = (account) => {
+    const { data, space } = account;
+    const buffer = Buffer.from(data);
+    const length = space / 80;
+    return seq(
+        SANCTUM_LST_STATE,
+        length,
+        "lstStateList"
+    ).decode(buffer);
+}
+
+
+module.exports = {
+    parseSanctumLstStateList,
+};
